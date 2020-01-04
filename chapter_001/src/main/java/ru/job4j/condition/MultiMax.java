cda46d0cf3a0f46b166e325f3fdad61a343ec763
Faiziev Salavat <@faiziev.s.m@gmail.com>
2020-01-04 20:41:15 +0500

    Максимум из трех чисел[#217672]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+       int res = first>second ? first : second;
+       int result = res>third ? res: third;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(11, 4, 2);
+        assertThat(result, is(11));
+    }
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 22);
+        assertThat(result, is(22));
+    }
+    @Test
+    public void whenEqual() {
+        MultiMax check = new MultiMax();
+        int result = check.max(11, 11, 11);
+        assertThat(result, is(11));
+    }
+}
