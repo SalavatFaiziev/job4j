4d2fd174c6ff8eb3a2fcb93adb0520bb64b1e49e
Faiziev Salavat <@faiziev.s.m@gmail.com>
2020-01-07 01:18:50 +0500

    Простые числа.[#217737]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish){
+        int count = 0;
+        CheckPrimeNumber ch=new CheckPrimeNumber();
+        for (int i = 2; i <= finish; i++) {
+            if (ch.check(i))
+                count++;
+        }
+        return count;
+
+    }
+}
