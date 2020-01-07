b35a312ccc01fe8c9680ee374665c2f11d554d08
Faiziev Salavat <@faiziev.s.m@gmail.com>
2020-01-07 22:01:31 +0500

    JavaDoc. Code convention[#217744]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+/**
+ * Сalculate class.
+ * @author Faiziev Salavat (faiziev.s.m@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Calculate {
+    /**
+     *Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "echo" + name;
+    }
+    /**
+     * Main method.
+     * @param args args.
+     */
+    public static void main(String[] args) {
+        }
+    }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+package ru.job4j.calculate;
+/**
+ * Package for calculate task.
+ *
+ * @author Faiziev Salavat (faiziev.s.m@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
