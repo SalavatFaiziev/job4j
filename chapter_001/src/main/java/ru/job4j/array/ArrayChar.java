2d3f5cd2f738d316b7b34f995afe3e17f0ca12ec
Faiziev Salavat <@faiziev.s.m@gmail.com>
2020-01-10 00:37:31 +0500

    Слово начинается с ...[#217679]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+/**
+ * Check word by chars in array
+ * @author Faiziev Salavat
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+        for (int i=0; i<pref.length; i++) {
+            if (word[i]==pref[i]) {
+                result=true;
+            }
+            else {
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result =ArrayChar.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void test1() {
+        char[] word = {'C', 'o', 'm', 'p', 'u', 't', 'e', 'r'};
+        char[] pref = {'C', 'o', 'm', 'p', 'u', 't', 't', 'r'};
+        boolean result =ArrayChar.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+}
