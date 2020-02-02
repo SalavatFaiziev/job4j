b58a06e8d09b4bbe09e6a8d45b7833417e5bddde
Faiziev Salavat <@faiziev.s.m@gmail.com>
2020-02-02 22:47:19 +0500

    Взаимодействие объектов.[#217828]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public  void roll(Ball ball) {
+        System.out.println("и покатился колобок дальше...");
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Fox.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Fox {
+    public void tryEat(Ball ball) {
+        System.out.println("не ешь меня, лиса!");
+    }
+
+    public void eat(Ball ball) {
+        System.out.println("вот и съела лиса колобка.");
+    }
+}
