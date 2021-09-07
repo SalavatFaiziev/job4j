c085d6b0e1bf5e02572b74a80e2a6e535303f925
Faiziev Salavat <@faiziev.s.m@gmail.com>
2021-09-07 15:39:24 +0500

    fix SqMax

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -1,21 +1,14 @@
 package ru.job4j.condition;
 
 public class SqMax {
-    public static int max(int first, int second, int third, int forth) {
-        int result = forth;
-        if (first > second) {
-            if (first > third) {
-                if (first > forth) {
-                    result = first;
-                }
-        } else if (second > third) {
-            if (second > forth) {
-                result = second;
-            }
-        } else if (third > forth) {
-            result = third;
-        }
-        }
-        return result;
+    public static int max(int left, int right) {
+        return left > right ? left : right;
     }
-}
+        public static int max(int first, int second, int third, int forth) {
+            return max(
+                    max(first, second),
+                    max(third, forth)
+            );
+        }
+    }
+
